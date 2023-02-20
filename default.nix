31f761756a2cea7b295b30826769ba66f881a71c
Daniel Lagos <dlagos@formbay.com.au>
2023-02-20 14:51:29 +1100

    Pin to nixos 22.11 release

--- default.nix
+++ default.nix
@@ -1,5 +1,9 @@
 let
-  pkgs = import <nixpkgs> {};
+  pkgs = import (builtins.fetchTarball {
+    name = "nixos-22.11";
+    url = "https://github.com/nixos/nixpkgs/archive/3824b383c8680f0a86eefb51e843bfce3000a713.tar.gz";
+    sha256 = "00jahw4wmg07ab4j90kzvq5qn3nyqfl3d8fy4ihblfr935365wdy";
+  }) {};
 
   timesheet = { mkDerivation, base, stdenv }:
     with pkgs.haskellPackages;
@@ -12,7 +16,7 @@ let
       buildDepends = [ pkgs.cabal-install pkgs.ghc ];
       executableHaskellDepends = [ base ];
       libraryHaskellDepends = [ attoparsec text random ];
-      license = stdenv.lib.licenses.bsd3;
+      license = pkgs.lib.licenses.bsd3;
     };
 in
   pkgs.haskellPackages.callPackage timesheet {}
