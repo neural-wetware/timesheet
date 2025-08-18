b8974639dec2da9e239a8501ddba102f39d60423
Daniel Lagos <daniel@example.com>
2025-08-18 11:51:04 +1000

    Convert to Flake using 25.05

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "Timesheet Haskell project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          hpkg = pkgs.haskellPackages;
+        in f system pkgs hpkg
+      );
+    in {
+      packages = forAllSystems (system: pkgs: hpkg:
+        {
+          timesheet = hpkg.developPackage {
+            root = ./.;
+            # This reads your cabal file and generates a derivation.
+            # You don’t need to manually list attoparsec/text/random etc.
+          };
+        }
+      );
+
+      # `nix build` defaults to building this
+      defaultPackage = forAllSystems (system: pkgs: hpkg:
+        self.packages.${system}.timesheet
+      );
+
+      devShells = forAllSystems (system: pkgs: hpkg:
+        {
+          default = hpkg.shellFor {
+            packages = p: [ self.packages.${system}.timesheet ];
+            # tools for hacking:
+            buildInputs = [
+              pkgs.cabal-install
+              pkgs.haskell-language-server
+              pkgs.ghcid
+            ];
+          };
+        }
+      );
+    };
+}
